31e3e31e5de476cdfc34de1602ff57b6ecc7f63b
Michael Wisely <michaelwisely@gmail.com>
2015-04-08 00:16:12 -0500

    Add some documentation to build.js

--- build.js
+++ build.js
@@ -1,3 +1,17 @@
+// This is a build configuration for `r.js`: the require.js optimizer.
+//
+// You can run it like this: `node_modules/.bin/r.js -o build.js`
+//
+// It will build compressjs by rolling it (and its modules) into one
+// big file. The included wrapper adds `compressjs` to the global
+// scope of the page. It doesn't make use of any AMD magic.
+//
+// In order to build it, you'll need to run `bower` to get a copy of
+// `almond.js`. That guy will be added to the optimized output, and
+// (with the help of the wrapper) will make all of the modules play
+// nice and place `compressjs` in the DOM so that you can get to it
+// like a plain ol', vanilla javascript script.
+
 ({
   baseUrl: ".",
   name: "bower_components/almond/almond",
